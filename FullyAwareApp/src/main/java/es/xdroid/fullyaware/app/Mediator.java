468f439a5b405217153e90ba88e303330b902eb0
Damian <damiansantae@gmail.com>
2017-03-01 18:38:52 +0000

    Eliminado paquete conflictivo

--- FullyAwareApp/src/main/java/es/xdroid/fullyaware/app/Mediator.java
+++
@@ -1,9 +0,0 @@
-package es.xdroid.fullyaware.app;
-
-import es.ulpgc.eite.clean.mvp.sample.dummy.Dummy;
-import es.ulpgc.eite.clean.mvp.sample.listToDo.ListToDo;
-
-public interface Mediator {
-  void startingDummyScreen(Dummy.ToDummy presenter);
-  void startingListToDoScreen(ListToDo.ToListToDo presenter);
-}
--- FullyAwareApp/src/main/java/es/xdroid/fullyaware/app/Navigator.java
+++
@@ -1,7 +0,0 @@
-package es.xdroid.fullyaware.app;
-
-import es.ulpgc.eite.clean.mvp.sample.dummy.Dummy;
-
-public interface Navigator {
-  void goToNextScreen(Dummy.DummyTo presenter);
-}
