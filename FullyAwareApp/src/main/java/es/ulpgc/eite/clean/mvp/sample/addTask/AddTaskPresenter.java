cf2ba26a98ef9e253f3bc208085b14391216088e
IvanGlez1996 <ivanglez1996@gmail.com>
2017-05-23 13:02:41 +0100

    Task addition to the device's default Calendar

--- FullyAwareApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/addTask/AddTaskPresenter.java
+++ FullyAwareApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/addTask/AddTaskPresenter.java
@@ -3,25 +3,40 @@ package es.ulpgc.eite.clean.mvp.sample.addTask;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.provider.CalendarContract;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.widget.DatePicker;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
+import java.util.UUID;
+import java.util.jar.Manifest;
 
 import es.ulpgc.eite.clean.mvp.ContextView;
 import es.ulpgc.eite.clean.mvp.GenericActivity;
 import es.ulpgc.eite.clean.mvp.GenericPresenter;
 import es.ulpgc.eite.clean.mvp.sample.NotificationService;
+import es.ulpgc.eite.clean.mvp.sample.R;
+import es.ulpgc.eite.clean.mvp.sample.app.App;
 import es.ulpgc.eite.clean.mvp.sample.app.Mediator;
 import es.ulpgc.eite.clean.mvp.sample.app.Navigator;
 import es.ulpgc.eite.clean.mvp.sample.app.Subject;
 import es.ulpgc.eite.clean.mvp.sample.realmDatabase.DatabaseFacade;
+
+import static android.support.v4.content.PermissionChecker.checkPermission;
+import static android.support.v4.content.PermissionChecker.checkSelfPermission;
 
 public class AddTaskPresenter extends GenericPresenter
     <AddTask.PresenterToView, AddTask.PresenterToModel, AddTask.ModelToPresenter, AddTaskModel>
@@ -150,6 +165,7 @@ public class AddTaskPresenter extends GenericPresenter
     String date = getDate();
     String deadline = getDeadLine(time,date);
     database.addTask(subject, title, description, deadline, "ToDo");
+    addEvent(title, subjectName, deadline);
       Navigator app = (Navigator)getView().getApplication();
       app.goToListToDoScreen(this);
       Context context = getApplicationContext();
@@ -181,9 +197,107 @@ public class AddTaskPresenter extends GenericPresenter
         Navigator app = (Navigator)getView().getApplication();
         app.startActivy(intent);
 
+    }
 
+
+  public Cursor getUserCalendar() {
+    if (ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
+      // TODO: Consider calling
+      //    ActivityCompat#requestPermissions
+      // here to request the missing permissions, and then overriding
+      //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+      //                                          int[] grantResults)
+      // to handle the case where the user grants the permission. See the documentation
+      // for ActivityCompat#requestPermissions for more details.
+
+      return null;
 
     }
+     final String[] EVENT_PROJECTION = new String[] {
+            CalendarContract.Calendars._ID,                           // 0
+            CalendarContract.Calendars.ACCOUNT_NAME,                  // 1
+            CalendarContract.Calendars.CALENDAR_DISPLAY_NAME,         // 2
+            CalendarContract.Calendars.OWNER_ACCOUNT                  // 3
+    };
+
+// The indices for the projection array above.
+    final int PROJECTION_ID_INDEX = 0;
+    final int PROJECTION_ACCOUNT_NAME_INDEX = 1;
+    final int PROJECTION_DISPLAY_NAME_INDEX = 2;
+    final int PROJECTION_OWNER_ACCOUNT_INDEX = 3;
+
+    Cursor cursor = getApplicationContext().getContentResolver().query(CalendarContract.Calendars.CONTENT_URI,
+            EVENT_PROJECTION,
+            null, null, null);
+    return cursor;
+  }
+
+  public void addEvent(String title, String subject, String deadline) {
+    String day = deadline.substring(0, 2);
+    int intDay = Integer.parseInt(day);
+
+    String month = deadline.substring(3, 5);
+    int intMonth = Integer.parseInt(month)-1;
+
+    String year = deadline.substring(6, 10);
+    int intYear = Integer.parseInt(year)-1;
+
+    String hour = deadline.substring(13, 15);
+    int intHour = Integer.parseInt(hour);
+
+    int intEndHour = Integer.parseInt(hour)+1;
+
+    String minutes = deadline.substring(16);
+    int intMinutes = Integer.parseInt(minutes);
+
+    TimeZone timeZone = TimeZone.getDefault();
+    String stringTimeZone = timeZone.getID();
+
+    Date dtStart = new Date(intYear, intMonth, intDay, intHour, intMinutes);
+
+    Date dtEnd = new Date(intYear, intMonth, intDay, intEndHour, intMinutes);
+
+    Cursor cursor = getUserCalendar();
+    cursor.moveToFirst();
+
+    if(getApplicationContext() != null) {
+    }
+
+    ContentResolver cr = getApplicationContext().getContentResolver();
+
+    ContentValues values = new ContentValues();
+    values.put(CalendarContract.Events.DTSTART, dtStart.getTime());
+    values.put(CalendarContract.Events.DTEND, dtEnd.getTime());
+    values.put(CalendarContract.Events.TITLE, title);
+    values.put(CalendarContract.Events.DESCRIPTION, subject + " - " + deadline);
+    values.put(CalendarContract.Events.CALENDAR_ID, Integer.parseInt(cursor.getString(cursor.getColumnIndex(CalendarContract.Calendars._ID))));
+    values.put(CalendarContract.Events.EVENT_TIMEZONE, stringTimeZone.toString());
+    if (ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.WRITE_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
+      // TODO: Consider calling
+      //    ActivityCompat#requestPermissions
+      // here to request the missing permissions, and then overriding
+      //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+      //                                          int[] grantResults)
+      // to handle the case where the user grants the permission. See the documentation
+      // for ActivityCompat#requestPermissions for more details.
+
+      return;
+    }
+    Uri uri = cr.insert(CalendarContract.Events.CONTENT_URI, values);
+
+    String stringEventID = uri.getLastPathSegment();
+    long eventID = Long.parseLong(stringEventID);
+
+
+    /*ContentResolver cr2 = getApplicationContext().getContentResolver();
+    ContentValues values2 = new ContentValues();
+    values2.put(CalendarContract.Reminders.MINUTES, 1);
+    values2.put(CalendarContract.Reminders.EVENT_ID, eventID);
+    values2.put(CalendarContract.Reminders.METHOD, CalendarContract.Reminders.METHOD_DEFAULT);
+    cr2.insert(CalendarContract.Reminders.CONTENT_URI, values);*/
+  }
+
+
 
     private String getDeadLine(String time, String date) {
         return date +" - "+ time;
@@ -327,5 +441,6 @@ public class AddTaskPresenter extends GenericPresenter
 List<Subject>list =database.getSubjectsFromDatabase();
   return list;
 }
+
 
 }
