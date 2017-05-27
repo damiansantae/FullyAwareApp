5eac9b89cece9903c0c92a9b38d2e279b0b6f9a4
Damian <damiansantae@gmail.com>
2017-05-27 16:58:40 +0100

    adding unit test

---
+++ FullyAwareApp/src/test/java/es/ulpgc/eite/clean/mvp/sample/ModelListTest.java
@@ -0,0 +1,42 @@
+package es.ulpgc.eite.clean.mvp.sample;
+
+import org.junit.Test;
+
+import es.ulpgc.eite.clean.mvp.sample.listToDoMaster.ListToDoModelMaster;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ModelListTest {
+
+    @Test
+    public void getCasesTwoWordsCapital_isCorrect() throws Exception {
+        ListToDoModelMaster modelMaster = new ListToDoModelMaster();
+        assertEquals("DA", modelMaster.calculateCases("Diseño de Aplicaciones"));
+    }
+    @Test
+    public void getCasesTwoWordsWithoutCapital_isCorrect() throws Exception {
+        ListToDoModelMaster modelMaster = new ListToDoModelMaster();
+        assertEquals("DA", modelMaster.calculateCases("dfiseño de aplicaciones"));
+    }
+
+    @Test
+    public void getCasesOneWordCapital_isCorrect() throws Exception {
+        ListToDoModelMaster modelMaster = new ListToDoModelMaster();
+        assertEquals("E", modelMaster.calculateCases("English"));
+    }
+
+    @Test
+    public void getCases1WordsWithoutCapital_isCorrect() throws Exception {
+        ListToDoModelMaster modelMaster = new ListToDoModelMaster();
+        assertEquals("E", modelMaster.calculateCases("english"));
+    }
+
+
+
+
+}
