90da25f7160383c4dd1342ba84fbe1926a3fc665
IvanGlez1996 <ivanglez1996@gmail.com>
2017-05-27 17:57:16 +0100

    Forgotten Activity deleted.

--- FullyAwareApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/app/Navigator.java
+++ FullyAwareApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/app/Navigator.java
@@ -7,9 +7,6 @@ import es.ulpgc.eite.clean.mvp.sample.dummy.Dummy;
 import es.ulpgc.eite.clean.mvp.sample.listDoneDetail.ListDoneDetail;
 import es.ulpgc.eite.clean.mvp.sample.listDoneMaster.ListDoneMaster;
 import es.ulpgc.eite.clean.mvp.sample.listDoneMaster.ListDoneViewMasterTesting;
-import es.ulpgc.eite.clean.mvp.sample.listForgottenDetail.ListForgottenDetail;
-import es.ulpgc.eite.clean.mvp.sample.listForgottenMaster.ListForgottenMaster;
-import es.ulpgc.eite.clean.mvp.sample.listForgottenMaster.ListForgottenViewMaster;
 import es.ulpgc.eite.clean.mvp.sample.listSubjects.ListSubject;
 import es.ulpgc.eite.clean.mvp.sample.listSubjects.ListSubjectPresenter;
 import es.ulpgc.eite.clean.mvp.sample.listSubjects.ListSubjectView;
@@ -30,11 +27,7 @@ public interface Navigator {
 
     void goToAddTaskScreen(ListToDoMaster.ListToDoTo presenter);
 
-    void backToMasterScreen(ListForgottenDetail.DetailToMaster presenter);
-
     void goToListDoneScreen(ListToDoMaster.ListToDoTo presenter);
-
-    void goToListForgottenScreen(ListToDoMaster.ListToDoTo presenter);
 
     void goToScheduleScreen(ListToDoMaster.ListToDoTo presenter);
 
@@ -48,7 +41,6 @@ public interface Navigator {
 
     /***********************************************************************
      ******** Navegación direccion ListDone al resto de pantallas y detalle*/
-    void goToListForgottenScreen(ListDoneMaster.ListDoneTo presenter);
     void goToListToDoScreen(ListDoneMaster.ListDoneTo presenter);
     void goToScheduleScreen(ListDoneMaster.ListDoneTo presenter);
     void goToPreferencesScreen(ListDoneMaster.ListDoneTo presenter);
@@ -66,13 +58,7 @@ public interface Navigator {
 
     /****************************************************************************
      ******** Navegación direccion ListForgotten al resto de pantallas y detalle*/
-    void goToScheduleScreen(ListForgottenMaster.ListForgottenTo presenter);
-    void goToListToDoScreen(ListForgottenMaster.ListForgottenTo presenter);
     void goToPreferencesScreen(Schedule.ScheduleTo presenter);
-  void goToListDoneScreen(ListForgottenMaster.ListForgottenTo presenter);
-
-    //Comunicación M/D
-    void goToDetailScreen(ListForgottenMaster.MasterListToDetail listForgottenPresenterMaster, ListForgottenViewMaster.TaskRecyclerViewAdapter adapter);
 
 
     /***************************************************************
@@ -82,19 +68,14 @@ public interface Navigator {
 
     void goToListDoneScreen(Schedule.ScheduleTo presenter);
 
-    void goToListForgottenScreen(Schedule.ScheduleTo presenter);
-
-
     void goToPreferencesScreen(Preferences.PreferencesTo presenter);
 
     void goToPreferencesScreen(ListToDoMaster.ListToDoTo presenter);
 
-    void goToPreferencesScreen(ListForgottenMaster.ListForgottenTo presenter);
 
     /*******************************************************************
      ******** Navegación direccion Preferences al resto de pantallas*/
     void goToListToDoScreen(AddTaskPresenter addTaskPresenter);
-
 
     void goToChangeColourDialog(PreferencesPresenter preferencesPresenter);
 
@@ -107,7 +88,6 @@ public interface Navigator {
     void goToListToDoScreen(ListSubject.ListSubjectTo presenter);
     void goToListSubjectScreen(ListSubjectPresenter presenter);
     void goToListDoneScreen(ListSubject.ListSubjectTo presenter);
-    void goToListForgottenScreen(ListSubject.ListSubjectTo presenter);
     void goToPreferencesScreen(ListSubject.ListSubjectTo presenter);
     void goToDetailScreen(ListSubjectPresenter listSubjectPresenter, ListSubjectView.SubjectRecyclerViewAdapter adapter);
 
